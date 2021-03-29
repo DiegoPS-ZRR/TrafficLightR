69829803d8831c3b9299dd52d42d80e357a12ad1
DiegoPS-ZRR <pacheco.luisdiego@gmail.com>
2021-03-29 23:32:56 +0000

    Done Traffic Light React

---
+++ src/js/component/TrafficLight.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+export const TrafficLight = () => {
+	const [color, setColor] = useState("red");
+
+	return (
+		<div className="box my-5">
+			<div className="box bg-dark p-3 rounded">
+				<div
+					className={
+						"light red " + (color == "red" ? "selected" : "")
+					}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"light yellow " + (color == "yellow" ? "selected" : "")
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"light green " + (color == "green" ? "selected" : "")
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+		</div>
+	);
+};
